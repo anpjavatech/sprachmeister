68e4080c0b31e7271e4f61eab4e99fd4a4d9cd60
Anoop Kuttikattu Sukumaran Nair <anoop.ks007@gmail.com>
2024-05-05 21:41:38 +0200

    Commit after addding the CORS access in backend.

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import userRouter from './src/routers/user.js'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.BACKEND_PORT
+
+// Allow requests from specific origins
+app.use(cors({
+    origin: '*'
+  }));
 
 app.use(express.json())
 app.use(userRouter)
