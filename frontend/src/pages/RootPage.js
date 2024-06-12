afdc13dc2ffa38948ac6d840abbea029b887ed3d
Anoop Kuttikattu Sukumaran Nair <anoop.ks007@gmail.com>
2024-06-12 11:09:29 +0200

    authentication page load when token expired + image renamed

--- frontend/src/pages/RootPage.js
+++ frontend/src/pages/RootPage.js
@@ -1,7 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../utils/auth";
 
 function RootPage() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
+
   return (
     <>
       <MainNavigation />
